eed8e7d025967c9620b33b2a8c761e3989307cf4
anatevs <anatevs@gmail.com>
2025-12-11 02:18:08 +0300

    refactor: separate class with test tasks

--- Task2_TaskManager/Program.cs
+++ Task2_TaskManager/Program.cs
@@ -1,21 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
-using Task2_TaskManager.Enums;
 using Task2_TaskManager.Inits;
 using Task2_TaskManager.TaskItems;
 
 var tasks = new TasksList();
 
-var tArray = new TaskItem[7];
-
-tArray[0] = new TaskItem("t1", "", Priority.High, Category.Work, Status.New);
-tArray[1] = new TaskItem("t2", "", Priority.Medium, Category.Home, Status.InProgress);
-tArray[2] = new TaskItem("t3", "", Priority.Low, Category.Study, Status.New);
-tArray[3] = new TaskItem("t4", "", Priority.Low, Category.Other, Status.Done);
-tArray[4] = new TaskItem("t5", "", Priority.High, Category.Home, Status.Done);
-tArray[5] = new TaskItem("t6", "", Priority.Medium, Category.Study, Status.InProgress);
-tArray[6] = new TaskItem("t7", "", Priority.Medium, Category.Work, Status.New);
-
-tasks.AddTasks(tArray);
+//tasks = TestTaskList.GetTasks();
 
 var mainMenu = new MainMenuInit(tasks);
 var mainCommands = mainMenu.Init();
