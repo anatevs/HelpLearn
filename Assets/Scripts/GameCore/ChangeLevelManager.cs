ce06c92b71387c7a04ae345bb9ad56f92ea751e0
anatevs <anatevs@gmail.com>
2025-12-16 16:50:47 +0300

    feat, chore: go to start menu, old Input in PlayerSettings

---
+++ Assets/Scripts/GameCore/ChangeLevelManager.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace GameCore
+{
+    public class ChangeLevelManager : MonoBehaviour
+    {
+        private int _startSceneIndex = 0;
+
+        private void Update()
+        {
+            if (Input.GetKeyUp(KeyCode.Escape))
+            {
+                SetStartScene();
+            }
+        }
+
+        private void SetStartScene()
+        {
+            SceneManager.LoadScene(_startSceneIndex);
+        }
+    }
+}
