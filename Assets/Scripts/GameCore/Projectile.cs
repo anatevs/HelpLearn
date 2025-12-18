dbe49f4573bcff1aa8b4f7634a984d8037824f63
anatevs <anatevs@gmail.com>
2025-12-18 15:28:53 +0300

    feat: projectile and it's spawn

---
+++ Assets/Scripts/GameCore/Projectile.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace GameCore
+{
+    public sealed class Projectile : MonoBehaviour
+    {
+        [SerializeField]
+        private float _speed = 5f;
+
+        private void Update()
+        {
+            transform.Translate(Vector3.forward * _speed * Time.deltaTime);
+        }
+    }
+}
---
+++ Assets/Scripts/GameCore/ProjectileSpawner.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace GameCore
+{
+    public sealed class ProjectileSpawner : MonoBehaviour
+    {
+        [SerializeField]
+        private Projectile _prefab;
+
+        [SerializeField]
+        private Transform _poolTransform;
+
+        private int _initCount = 10;
+
+        private Queue<Projectile> _pool = new();
+
+        private void Awake()
+        {
+            for (int i = 0; i < _initCount; i++)
+            {
+                var item = Instantiate(_prefab, _poolTransform);
+                item.gameObject.SetActive(false);
+
+                _pool.Enqueue(item);
+            }
+        }
+
+        [SerializeField]
+        bool _spawn = true;
+        [SerializeField]
+        bool _unspawn = true;
+        Projectile _current;
+        private void Update()
+        {
+            if (_spawn)
+            {
+                _current = Spawn(Vector3.forward, Quaternion.identity);
+                _spawn = false;
+            }
+            if (_unspawn && _current != null)
+            {
+                Unspawn(_current);
+                _unspawn = false;
+            }
+        }
+
+        public Projectile Spawn(Vector3 position, Quaternion rotation)
+        {
+            if (!_pool.TryDequeue(out var item))
+            {
+                item = Instantiate(_prefab);
+            }
+
+            item.transform.SetParent(transform);
+            item.transform.position = position;
+            item.transform.rotation = rotation;
+            item.gameObject.SetActive(true);
+
+            return item;
+        }
+
+        public void Unspawn(Projectile item)
+        {
+            item.gameObject.SetActive(false);
+            _pool.Enqueue(item);
+        }
+    }
+}
